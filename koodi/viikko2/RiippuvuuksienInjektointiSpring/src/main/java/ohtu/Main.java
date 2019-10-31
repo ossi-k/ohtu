e051ca0db237683ef4cac875dc2cefaa5ac0914c
Matti Luukkainen <mluukkai@iki.fi>
2019-11-01 00:26:05 +0200

    laskari 2

--- koodi/viikko2/RiippuvuuksienInjektointiSpring/src/main/java/ohtu/Main.java
+++ koodi/viikko2/RiippuvuuksienInjektointiSpring/src/main/java/ohtu/Main.java
@@ -1,14 +1,13 @@
 package ohtu;
 
-import ohtu.laskin.IO;
 import ohtu.laskin.Laskin;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = new FileSystemXmlApplicationContext("src/main/resources/spring-context.xml");
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
         
         Laskin laskin = ctx.getBean(Laskin.class);
         laskin.suorita();
