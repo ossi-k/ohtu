a37b4a237995c6ace21bc3d422c0c6d51e2c89a5
ossi-k <ossi.kohvakka@gmail.com>
2020-02-13 18:22:01 +0200

    Tehtävä 2: Sortataan suomalaiset pelaajat pisteiden mukaan ja tulostetaan lista

--- koodi/viikko3/nhlreader/src/main/java/ohtu/Main.java
+++ koodi/viikko3/nhlreader/src/main/java/ohtu/Main.java
@@ -2,9 +2,22 @@ package ohtu;
 
 import java.io.IOException;
 import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import org.apache.http.client.fluent.Request;
 
 public class Main {
+
+    public static class SortByPoints implements Comparator<Player> {
+
+        @Override
+        public int compare(Player a, Player b) {
+            return Integer.compare(a.getPoints(), b.getPoints());
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         String url = "https://nhlstatisticsforohtu.herokuapp.com/players";
@@ -20,6 +33,28 @@ public class Main {
         System.out.println("Oliot:");
         for (Player player : players) {
             System.out.println(player);
+        }
+        
+
+        System.out.println("=============================");
+
+        ArrayList<Player> finnishPlayers = new ArrayList<>();
+        
+        for (Player player : players) {
+            if (player.getNationality().equals("FIN")) {
+                finnishPlayers.add(player);
+            }
+        }
+        
+        Collections.sort(finnishPlayers, new SortByPoints());
+        Collections.reverse(finnishPlayers);
+        
+        System.out.println("Finnish Players sorted by goals and assists");
+        for (Player player : finnishPlayers) {
+            if (player.getNationality().equals("FIN")) {
+                System.out.println(player.getName() + " " + player.getTeam() + " " + 
+                        player.getGoals() + " + " + " " + player.getAssists() + " = " + player.getPoints());
+            }
         }
     }
 }
--- koodi/viikko3/nhlreader/src/main/java/ohtu/Player.java
+++ koodi/viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -1,11 +1,15 @@
 
 package ohtu;
 
-public class Player {
+import java.util.Comparator;
+
+public class Player implements Comparator<Player>{
     private String name;
     private String team;
+    private String nationality;
     private int goals;
     private int assists;
+    private int points;
 
     public void setName(String name) {
         this.name = name;
@@ -14,10 +18,41 @@ public class Player {
     public String getName() {
         return name;
     }
+    
+    public String getNationality() {
+        return nationality;
+    }
 
+    public int getGoals() {
+        return goals;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+    
+    public int getPoints() {
+        points = this.goals+this.assists;
+        return points;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+    
     @Override
     public String toString() {
-        return name + " team " + team + " goals " + goals + " assists " + assists;
+        return name + " " + nationality + " team " + team + " goals " + goals + " assists " + assists;
     }
-      
+    
+    @Override
+    public int compare(Player a, Player b) {
+        int returnValue = 0;
+        if (a.goals < b.goals) {
+            returnValue = -1;
+        } else {
+            returnValue = 1;
+        }
+        return returnValue;
+    }
 }
